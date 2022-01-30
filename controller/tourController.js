c9e94ab67a5cd7e8c62b1e4bda927c9cd22da80e
kangdan <jayadanielsutra@gmail.com>
2022-01-30 11:32:33 +0700

    add promise all in handle image upload

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,6 +1,6 @@
 const Tour = require('../models/tourModel');
 const factory = require('./factoryController');
-// const { saveImageToStorage } = require('../helper/utils');
+const { saveImageToStorage } = require('../helper/utils');
 
 exports.createTour = factory.create(Tour);
 exports.deleteTour = factory.deleteById(Tour);
@@ -8,8 +8,10 @@ exports.getTour = factory.getOneById(Tour);
 exports.getTours = factory.getAll(Tour);
 exports.updateTour = factory.updateById(Tour);
 exports.handleImageUpload = async (req, res, next) => {
+  console.log('start');
   if (!req.files) return next();
-  // const currentUserId = req.authUser.id;
+  const currentUserId = req.authUser.id;
+
   // Object.values(req.files).forEach((field) => {
   //   const { fieldname } = field[0];
   //   req.body[fieldname] = [];
@@ -18,8 +20,38 @@ exports.handleImageUpload = async (req, res, next) => {
   //     req.body[fieldname].push(imageUrl);
   //   });
   // });
-  console.log(req.files);
-  // const result = req.files.map((field) => field);
-  // console.log(result);
+  // const result = (() =>
+  //   new Promise((resolve) => {
+  //     setTimeout(() => {
+  //       resolve('halloo');
+  //     }, req.body.time * 1);
+  //   }))();
+
+  // const result = (files, userId) =>
+  //   new Promise((resolve) => {
+  //     Object.values(files).forEach(async (field) => {
+  //       field.forEach(async (file, index) => {
+  //         const imageUrl = await saveImageToStorage(file, userId, index);
+  //         resolve(imageUrl);
+  //       });
+  //     });
+  //   });
+  // console.log(await result(req.files, currentUserId));
+
+  const photos = req.files.imagePhotos;
+
+  const all = () =>
+    Promise.all(
+      photos.map(
+        async (file, index) =>
+          await saveImageToStorage(file, '61c37a2659d469bcdc271d05', index),
+      ),
+    );
+
+  const result = await all();
+
+  console.log(result);
+
+  console.log('end');
   res.end('end');
 };
