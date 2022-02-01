905509d3fe61944075c5cdc1637d09c196b24245
kangdan <jayadanielsutra@gmail.com>
2022-02-01 10:12:05 +0700

    setting env for storage

--- helper/storage.js
+++ helper/storage.js
@@ -1,13 +1,13 @@
 const cloud = require('@google-cloud/storage');
 const path = require('path');
 
-const serviceKey = path.join(__dirname, '../keys/wishta-storage.json');
+const serviceKey = path.join(__dirname, process.env.STORAGE_KEY_DIRECTORY);
 
 const { Storage } = cloud;
 const storage = new Storage({
   keyFilename: serviceKey,
   // projectId: 'wishta'
 });
-const bucket = storage.bucket('wishta');
+const bucket = storage.bucket(process.env.STORAGE_BUCKET_NAME);
 
 module.exports = bucket;
